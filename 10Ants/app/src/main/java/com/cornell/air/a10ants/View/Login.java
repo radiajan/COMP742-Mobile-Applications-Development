2ebb28a538d5abd4372c7e6bfe43e61c73075c2c
rodrigoadati <rodrigo_adati@hotmail.com>
2017-05-11 23:28:31 +1200

    Updated report screen

--- 10Ants/app/src/main/java/com/cornell/air/a10ants/View/Login.java
+++ 10Ants/app/src/main/java/com/cornell/air/a10ants/View/Login.java
@@ -1,28 +1,23 @@
 package com.cornell.air.a10ants.View;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
 import com.cornell.air.a10ants.Menu.MenuFrame;
 import com.cornell.air.a10ants.R;
 
-/**
- * Created by root on 11/05/17.
- */
-
 public class Login extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.login_screen);
     }
 
     public void loginGoogle(View view){
         Intent intent = new Intent(view.getContext(), MenuFrame.class);
         startActivity(intent);
     }
-
-
 }
