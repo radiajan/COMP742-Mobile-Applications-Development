10a4eeadb14fb760803dc5f0c3d7130ac12c79c3
rodrigoadati <rodrigo_adati@hotmail.com>
2017-05-11 20:40:28 +1200

    added login as main

---
+++ 10Ants/app/src/main/java/com/cornell/air/a10ants/View/Login.java
@@ -0,0 +1,29 @@
+package com.cornell.air.a10ants.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.cornell.air.a10ants.Menu.MainActivity;
+import com.cornell.air.a10ants.R;
+
+/**
+ * Created by root on 11/05/17.
+ */
+
+public class Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void loginGoogle(View view){
+        Intent intent = new Intent(view.getContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
