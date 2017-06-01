9bd36eec61513c5a3ba5daa0f97ca589e7d643e2
rodrigoadati <rodrigo_adati@hotmail.com>
2017-06-02 04:06:20 +1200

    UI changes and working auth

--- 10Ants/app/src/main/java/com/cornell/air/a10ants/View/addExpense.java
+++ 10Ants/app/src/main/java/com/cornell/air/a10ants/View/addExpense.java
@@ -1,9 +1,12 @@
 package com.cornell.air.a10ants.View;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -12,6 +15,10 @@ import com.cornell.air.a10ants.DAL.ExpenseDAL;
 import com.cornell.air.a10ants.Model.Expense;
 import com.cornell.air.a10ants.R;
 import com.google.firebase.database.DatabaseReference;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * Created by root on 8/05/17.
@@ -22,10 +29,46 @@ public class addExpense extends AppCompatActivity {
     ExpenseDAL expenseDAL;
     Expense expense;
 
+    //Instance variables
+    Calendar myCalendar = Calendar.getInstance();
+
+    //Instance controls
+    EditText etPaidOn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_expenses);
+
+        etPaidOn = (EditText) findViewById(R.id.etPaidOn);
+        etPaidOn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(addExpense.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etPaidOn.setText(sdf.format(myCalendar.getTime()));
     }
 
     /**
@@ -59,13 +102,4 @@ public class addExpense extends AppCompatActivity {
             Toast.makeText(this, "Expense unsuccessfull", Toast.LENGTH_SHORT).show();
         }
     }
-
-    /**
-     *
-     * TODO: edit and delete
-     * Separate the expense VIEW and the expense class
-     * Check the validation
-     * add listing method based on property, tenant
-     * Javadocs
-     */
 }
--- 10Ants/app/src/main/java/com/cornell/air/a10ants/View/addTenant.java
+++ 10Ants/app/src/main/java/com/cornell/air/a10ants/View/addTenant.java
@@ -1,11 +1,13 @@
 package com.cornell.air.a10ants.View;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -17,6 +19,10 @@ import com.cornell.air.a10ants.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 /**
  * Created by root on 8/05/17.
  */
@@ -25,12 +31,11 @@ public class addTenant extends AppCompatActivity {
     //Reference to the property database
     DatabaseReference databaseTenants;
 
+    //Instance variables
+    Calendar myCalendar = Calendar.getInstance();
+
     //Reference of the controllers
-    EditText etName;
-    EditText etEmail;
-    EditText etPhone;
     EditText etDateofBirth;
-    Spinner spProperty;
 
     //Class instance
     Tenant tenant;
@@ -40,6 +45,36 @@ public class addTenant extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_tenant);
+
+        etDateofBirth = (EditText) findViewById(R.id.etDateofBirth);
+        etDateofBirth.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(addTenant.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        etDateofBirth.setText(sdf.format(myCalendar.getTime()));
     }
 
     /**
